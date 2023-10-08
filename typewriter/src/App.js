d5a7c8202b3d8b68e40e6784f35db259e89121af
ambika2404 <ambika.kumari@hotmail.com>
2023-10-08 14:09:55 -0700

    updated typewriter component

--- typewriter/src/App.js
+++ typewriter/src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import Typewriter from "./Typewriter";
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Typewriter />
     </div>
   );
 }
